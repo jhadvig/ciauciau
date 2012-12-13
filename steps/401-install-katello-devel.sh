ee515bc412760ac680e92fae4ec4729cda94e1b9
Tomas Strachota <tstrachota@redhat.com>
2012-12-13 14:54:21 +0100

    python-pep8 installation

--- steps/401-install-katello-devel.sh
+++ steps/401-install-katello-devel.sh
@@ -1,3 +1,3 @@
 yum install -y katello-devel-all
-yum install -y gcc-c++ gcc make ruby-devel postgresql-devel sqlite-devel libxml2 libxml2-devel libxslt libxslt-devel spacewalk-pylint
+yum install -y gcc-c++ gcc make ruby-devel postgresql-devel sqlite-devel libxml2 libxml2-devel libxslt libxslt-devel spacewalk-pylint python-pep8
 
